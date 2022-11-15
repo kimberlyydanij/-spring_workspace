a9b7e4f381f077201f7a86d164e5c3a007404bd7
kimberlyydanij <kimberlydanij@gmail.com>
2022-11-15 21:48:59 +0900

    20221115 | Add

--- spring_01_basic/src/sample01/HelloSpring.java
+++ spring_01_basic/src/sample01/HelloSpring.java
@@ -10,12 +10,17 @@ public class HelloSpring {
 
 	public static void main(String[] args) {
 		
+		//MessageBeanKorea bean = new MessageBeanKorea();
+		//display(bean);
 		
+		MessageBeanEnglish bean = new MessageBeanEnglish();
+		display(bean);		
+	
 
 	} // end main
 	
-	public static void display(MessageBeanKorea bean) {
-		bean.sayHello("스프링");
+	public static void display(MessageBeanEnglish bean) {
+		bean.sayHello("Spring");
 	} //end display
 
 } // end class
