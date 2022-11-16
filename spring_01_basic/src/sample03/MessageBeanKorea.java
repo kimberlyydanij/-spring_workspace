ca2856c3efae0d4bb5ac4b57f3e3e6018c8be5a6
kimberlyydanij <kimberlydanij@gmail.com>
2022-11-16 16:08:14 +0900

    20221116 | Add

---
+++ spring_01_basic/src/sample03/MessageBeanKorea.java
@@ -0,0 +1,9 @@
+package sample03;
+
+public class MessageBeanKorea implements MessageBean{
+	
+	public void sayHello(String name) {
+		System.out.printf("안녕, %s !!!",name);
+		
+	} // end sayHello
+}
