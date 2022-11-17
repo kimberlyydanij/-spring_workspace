6eadf84e568b2bda365a5b2a0173f58959eec19c
kimberlyydanij <kimberlydanij@gmail.com>
2022-11-17 19:40:52 +0900

    20221117 | Add

---
+++ Spring_04_mvc/src/main/java/part01/HelloController.java
@@ -0,0 +1,21 @@
+package part01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// http://localhost:8090/myapp/hello.htm
+
+@Controller  // 컨트롤러로 사용할 클래스에는 반드시 컨트롤러 어노테이션 선언을 해야 함
+public class HelloController {
+	
+	public HelloController() {
+		
+	}
+
+	@RequestMapping(value="hello.htm")
+	public String search() {
+			
+		return "part01/hello";
+	} // end search
+	
+} // end class
