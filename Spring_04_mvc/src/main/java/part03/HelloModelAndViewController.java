0f784a605b06dbc62b04b1a20455434e987d3c82
kimberlyydanij <kimberlydanij@gmail.com>
2022-11-17 21:41:10 +0900

    20221117 | Add

--- Spring_04_mvc/src/main/java/part03/HelloModelAndViewController.java
+++ Spring_04_mvc/src/main/java/part03/HelloModelAndViewController.java
@@ -1,12 +1,16 @@
 package part03;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.ModelAndViewDefiningException;
+
+// http://localhost:8090/myapp/helloModelAndView.htm
 
 @Controller
 public class HelloModelAndViewController {
 
+	@RequestMapping("/helloModelAndView.htm")
 	public ModelAndView search(ModelAndView mav) {
 		/*  public ModelAndView search(ModelAndView mav) {
 		 * = public ModelAndView search() {
@@ -15,9 +19,9 @@ public class HelloModelAndViewController {
 			
 		mav.addObject("name","kim");
 		mav.addObject("age",30);
-		mav.setViewName("part03/helloModelView");
+		mav.setViewName("part03/helloModelAndView");
 		
 		return mav;
-	}
+	}  // end search
 	
 } // end class
