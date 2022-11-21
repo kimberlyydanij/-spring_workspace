d9990eec921ac31945d659314ac8a304448f924a
kimberlyydanij <kimberlydanij@gmail.com>
2022-11-21 17:26:23 +0900

    20221121 | Add

--- spring_06_tx/src/main/java/part02/MemController.java
+++ spring_06_tx/src/main/java/part02/MemController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 // http://localhost:8090/myapp/firstmain.st
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+// http://localhost:8090/myapp/secondmain.ss
 @Controller
 public class MemController {
 	
@@ -18,12 +20,18 @@ public class MemController {
 		this.svc = svc;
 	} // end setSvc
 	
-	@RequestMapping("/firstmain.st")
+	@RequestMapping("/secondmain.ss")
 	public void execute() {
 		System.out.println("execute");
+		//트랜젝션이 설정된 메소드에서는 try~catch를 설정하며 ㄴ안되고
+		//메소드를 호출하는 곳에서  try~catch를 한다
+		
+		try {		
 		svc.insertProcess();
+		System.out.println("result OK");
+		} catch(Exception ex) {
+			System.out.println("result : " + ex.toString());
+		}
 	} // end execute
-	
-	
 	
 } // end class
--- spring_06_tx/src/main/java/part02/ServiceImp.java
+++ spring_06_tx/src/main/java/part02/ServiceImp.java
@@ -1,17 +1,11 @@
 package part02;
 
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
-
 import model.MemDAO;
 import model.MemDTO;
 
 public class ServiceImp implements Service{
 	
 	private MemDAO memDAO;
-	private TransactionTemplate transacionTemplate;
-	
 	
 	public ServiceImp() {
 	
@@ -20,27 +14,12 @@ public class ServiceImp implements Service{
 	public void setMemDAO(MemDAO memDAO) {
 		this.memDAO = memDAO;
 	}
-	
-	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
-		this.transacionTemplate = transactionTemplate;
-	}
-	
+
 	@Override
 	public void insertProcess() {
-		String result = transacionTemplate.execute(new TransactionCallback<String>() {
-			@Override
-			public String doInTransaction(TransactionStatus status) {
-	
-				try {
-				memDAO.insertMethod(new MemDTO(44, "용팔이", 50, "경기"));
-				memDAO.insertMethod(new MemDTO(45, "유대위", 20, "대전"));
-				return "OK";
-				} catch(Exception ex) {
-					status.setRollbackOnly();
-				return ex.toString();
-				}
-			}
-		});
-		System.out.println("Result : " + result);
+
+	memDAO.insertMethod(new MemDTO(49, "용팔이", 50, "경기"));
+	memDAO.insertMethod(new MemDTO(50, "유대위", 20, "대전"));
+
 	} // end insertProcess
 }
