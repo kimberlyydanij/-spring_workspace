d6a48605ec0eddf652b14e87e653ae97c95f5331
kimberlyydanij <kimberlydanij@gmail.com>
2022-11-28 17:45:18 +0900

    20221128 | Add

--- spring_12_interceptor/src/main/java/part03/controller/LoginController.java
+++ spring_12_interceptor/src/main/java/part03/controller/LoginController.java
@@ -1,7 +1,13 @@
 package part03.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import part03.dto.PersonDTO;
+
+//http://localhost:8090/myapp/login.do
 
 @Controller
 public class LoginController {
@@ -15,5 +21,16 @@ public class LoginController {
 		
 		return "part03/loginForm";
 	} // end loginProcess
+	
+	@RequestMapping("/logpro.do")
+	public String loginExecution(PersonDTO dto, HttpSession session) {
+		if(dto.getId().equals("kim") && dto.getPass().equals("1234")) {
+			session.setAttribute("chk", dto.getId());
+			session.setMaxInactiveInterval(1000*60*30);  // 세션 만료시간 지정, 1000=1초
+			System.out.println(session.getAttribute("chk"));		
+		}
+		return "redirect:/index.do";
+	}
+	
 
 } // end class
